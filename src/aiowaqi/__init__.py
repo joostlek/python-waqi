da073af6afd2fd6e1027ae166e8350d0a33238f3
Joost Lekkerkerker <joostlek@outlook.com>
2023-08-07 21:50:20 +0200

    Add getting stations by name (#6)

--- src/aiowaqi/__init__.py
+++ src/aiowaqi/__init__.py
@@ -1,17 +1,29 @@
 """Asynchronous Python client for the WAQI API."""
 
-from .exceptions import WAQIAuthenticationError, WAQIConnectionError, WAQIError
+from .exceptions import (
+    WAQIAuthenticationError,
+    WAQIConnectionError,
+    WAQIError,
+    WAQIUnknownCityError,
+    WAQIUnknownStationError,
+)
 from .models import (
     Attribution,
     City,
     Coordinates,
+    Location,
     WAQIAirQuality,
     WAQIExtendedAirQuality,
+    WAQISearchResult,
 )
 from .waqi import WAQIClient
 
 __all__ = [
     "WAQIError",
+    "WAQIUnknownStationError",
+    "WAQIUnknownCityError",
+    "Location",
+    "WAQISearchResult",
     "WAQIAuthenticationError",
     "WAQIClient",
     "WAQIConnectionError",
